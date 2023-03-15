badb0894fc23985cf7bc2e3f54bbda5e626d2259
Rachel Kim <hyunjinney@naver.com>
2023-03-15 12:24:01 +0900

    대화창 만들기
    + 플레이어 이동
    + 테스트용 객체들 추가함
    + 대화창 제작 중..

---
+++ OceanGame/Assets/Scripts/TalkManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TalkManager : MonoBehaviour
+{
+    Dictionary<int, string[]> talkData;
+
+    private void Awake()
+    {
+        talkData = new Dictionary<int, string[]>();
+        GenerateData();
+    }
+    private void GenerateData()
+    {
+        //npc
+        talkData.Add(1000, new string[] { "정신이 들었니?" });
+
+        //사물
+        talkData.Add(100, new string[] { "특별할 건 없어보인다." });
+
+        //첫번째 매개변수는 objData의 id 넣기
+    }
+
+    public string GetTalk(int id, int talkIndex) //talkindex는 몇번째문장을 가져올 것인지 결정
+    {
+        if (talkIndex == talkData[id].Length)
+            return null;
+        else
+            return talkData[id][talkIndex];
+    }
+}
